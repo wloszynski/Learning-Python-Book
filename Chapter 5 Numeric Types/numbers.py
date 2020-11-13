45c2084cb0b5250b20717a1afe285e61c50ce977
wloszynski <adrian@wloszynski.pl>
2020-11-13 13:11:43 +0100

    20th day of 66 days of Data

---
+++ Chapter 5 Numeric Types/numbers.py
@@ -0,0 +1,28 @@
+# decimal
+d = 123
+# hexa
+h = 0x1F2
+# octa
+o = 0o71
+# binary
+b = 0b1010
+
+print(d, h, o , b)
+
+# complex numbers
+print(complex(1, 2))
+
+a = 3
+b = 4
+
+print(a + 1, a - 1)
+print(b * 3, b / 2)
+print(a % 2, b ** 2)
+print(2 + 4.0, 2.0 ** b)
+
+print(b / (2.0 + a))
+
+print('1' == 1)
+
+# same as 1 == 2 and 2 < 3
+print(1 == 2 < 3)
